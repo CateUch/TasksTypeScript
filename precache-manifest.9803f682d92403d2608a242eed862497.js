a901aff7f38641e50a32eb1ac6c18163097f4a94
Cate Uch <cat.uch@yandex.ru>
2020-09-30 22:06:17 +0300

    Updates

---
+++ precache-manifest.9803f682d92403d2608a242eed862497.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dfd97299a9da966dd500f858a787ae3a",
+    "url": "/practice-tasks/index.html"
+  },
+  {
+    "revision": "bee33b0a689f17beb98c",
+    "url": "/practice-tasks/static/css/main.c4ab365c.chunk.css"
+  },
+  {
+    "revision": "0176888f4f47e5b35218",
+    "url": "/practice-tasks/static/js/2.1e524495.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/practice-tasks/static/js/2.1e524495.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bee33b0a689f17beb98c",
+    "url": "/practice-tasks/static/js/main.7adcd51d.chunk.js"
+  },
+  {
+    "revision": "adf75791731523470a28",
+    "url": "/practice-tasks/static/js/runtime-main.4eacbdd1.js"
+  }
+]);
